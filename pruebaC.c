c32def1fc6d4b93628fda67adfdd52bab52a9563
israelPlaza <israel.plaza.mayoral@gmail.com>
2024-09-17 18:46:35 +0200

    mejorando swcht

---
+++ pruebaC.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    printf("hola mundo\n");
+    int numero;
+    int otro=7;
+    int suma;
+    printf("Ingrese número: \n");
+    scanf("%i",&numero);
+    printf("La suma es:");
+    suma=numero+otro;
+    printf("%i\n",suma);
+    return 0;
+
+}
